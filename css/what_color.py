85a0287d05797d8779d0251483bfd192f820c7bc
AngLxd <mpena1464@gmail.com>
2024-09-26 16:46:50 -0600

    first commit

---
+++ css/what_color.py
@@ -0,0 +1,3 @@
+Favorite_color= input('Please enter your favorite color:')
+Second_favorite_color= input('please enter your second favorite color:')
+print('Your favorite colors are: ' + Favorite_color.capitalize() + ' and ' + Second_favorite_color.capitalize() + ', hooray!')
