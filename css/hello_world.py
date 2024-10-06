e9eac8d3848c132482bb00378cb64215b54c7c04
AngLxd <mpena1464@gmail.com>
2024-10-05 23:01:26 -0600

    index

---
+++ css/hello_world.py
@@ -0,0 +1,29 @@
+print('Hello world')
+
+
+
+
+
+
+
+name= input('What is your favorite color?')
+print('Your favorite color is'+ name)
+
+import tkinter as tk
+def display_color():
+    color=entry.get()
+    try:
+        label.config(bg=color)
+        label.config(text=f'Your favorite color is{color}!')
+    except tk.TclError:
+        label.config(text='Invalid Color', bg='white')
+window= tk.Tk()
+window.title('Favorite Color')
+window.geometry('300x200')
+entry= tk.Entry(window, width=20)
+entry.pack(pady=10)
+button=tk.Button(window, text='Submit', command=display_color)
+button.pack(pady=10)
+label=tk.Label(window, text='What is your favorite color?', bg='white', fg='black', font=('Helvetica',16))
+label.pack(expand=True, fill='both')
+window.mainloop()
---
+++ css/python_synthax.py
@@ -0,0 +1,2 @@
+age=int(input('Enter your age '))
+seconds= age * 60 * 60 * 24 * 365
