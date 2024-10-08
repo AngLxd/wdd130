45869cd7307757be58c3a4f3b88bd321b9aee98b
AngLxd <mpena1464@gmail.com>
2024-10-08 17:03:16 -0600

    index.html

---
+++ css/python_syntax.py
@@ -0,0 +1,21 @@
+#my_num= 2/3
+#print(f"My number is: {my_num:.3f}")
+
+#import math
+#my_num= 4
+#squared = math.pow(my_num, 10);
+#truncated = math.floor (my_num)
+
+#print(f'my number is: {truncated}')
+
+import math
+a= 7
+b= 13
+c= math.sqrt(a** 2 + b ** 2)
+
+print(f'C is {c}')
+
+a= 7
+b= 13
+c= math.hypot(a, b)
+print(f'c is {c}')
