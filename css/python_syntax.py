58c0d1ff329b905e5b6905b6a771c58c43fc3a30
AngLxd <mpena1464@gmail.com>
2024-10-10 23:46:21 -0600

    teaks.3

--- css/python_syntax.py
+++ css/python_syntax.py
@@ -8,14 +8,33 @@
 
 #print(f'my number is: {truncated}')
 
+#import math
+#a= 7
+#b= 13
+#c= math.sqrt(a** 2 + b ** 2)
+
+#print(f'C is {c}')
+
+#a= 7
+#b= 13
+#c= math.hypot(a, b)
+#print(f'c is {c}')
+#x = 2
+#y = 3
+#z = 4  
+
+#w = x + y * z  
+
+#print(w)
 import math
-a= 7
-b= 13
-c= math.sqrt(a** 2 + b ** 2)
-
-print(f'C is {c}')
-
-a= 7
-b= 13
-c= math.hypot(a, b)
-print(f'c is {c}')
+print (f"Welcome to the velocity calculator. Please enter the following:\n")
+m = float(input("Mass (in kg): "))
+g = float(input("Gravity (in m/s^2, 9.8 for Earth, 24 for Jupiter): "))
+t = float(input("Time (in seconds): "))
+p = float(input("Density of the fluid (in kg/m^3, 1.3 for air, 1000 for water): "))
+A = float(input("Cross sectional area (in m^2): "))
+C = float(input("Drag constant (0.5 for sphere, 1.1 for cylinder): "))
+c = (1 / 2) * p * A * C
+v = math.sqrt(m * g / c) * (1 - math.exp((-math.sqrt(m * g * c) / m) * t))
+print(f"The inner value of c is: {c} ")
+print(f"The velocity after 10.0 seconds is: {v} m/s")
--- css/python_synthax.py
+++ css/python_synthax.py
@@ -1,2 +1,9 @@
 age=int(input('Enter your age '))
 seconds= age * 60 * 60 * 24 * 365
+x = 2
+y = 3
+z = 4  
+
+w = x + y * z  
+
+print(w)
